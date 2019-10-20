e397fb33aab5a74558fa8bf3622f447c6fd46a9f
huwenqing <huwenqing@gzrqian.com>
2019-10-20 18:51:18 +0800

    Builder

---
+++ src/main/java/com/hwq/fundamment/Java_Desgin_Pattern/designPattern_Build/v2/Test.java
@@ -0,0 +1,13 @@
+package com.hwq.fundamment.Java_Desgin_Pattern.designPattern_Build.v2;
+
+/**
+ * @author hwq
+ * @date 2019/10/20
+ */
+public class Test {
+    public static void main(String[] args) {
+        Course couser = new Course.CourseBilder().courseBuildName("JAVA课程").buildCourseArticle("JAVA课程手记").buildCoursePPT("JAVA课程视频").buildCourseVideo(null).buildCourseQA(null).build();
+
+        System.out.println(couser);
+    }
+}
--- src/main/java/com/hwq/fundamment/test/Main.java
+++ src/main/java/com/hwq/fundamment/test/Main.java
@@ -1,22 +1,22 @@
-package com.hwq.fundamment.test;
-
-import com.alibaba.fastjson.JSONObject;
-import lombok.extern.slf4j.Slf4j;
-
-/**
- * @author hwq
- * @date 2018/10/27
- */
-@Slf4j
-public class Main {
-    public static void main(String[] args) {
-        Man<Person> man = new Man<Person>();
-
-        Person person = new Person();
-        person.setAge("10");
-        person.setName("fox");
-
-        Man man1 = man.toDo(person);
-        log.info("man1={}", JSONObject.toJSON(man1));
-    }
-}
+//package com.hwq.fundamment.test;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import lombok.extern.slf4j.Slf4j;
+//
+///**
+// * @author hwq
+// * @date 2018/10/27
+// */
+//@Slf4j
+//public class Main {
+//    public static void main(String[] args) {
+//        Man<Person> man = new Man<Person>();
+//
+//        Person person = new Person();
+//        person.setAge("10");
+//        person.setName("fox");
+//
+//        Man man1 = man.toDo(person);
+//        log.info("man1={}", JSONObject.toJSON(man1));
+//    }
+//}
