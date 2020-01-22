4111e38cbcae3a15ba611eb7edfc1a39579e72ab
“yhuwenqing “hwq8910@163,com <hwq8910@163.com>
2020-01-22 14:17:06 +0800

    'join()方法和生产者/消费者'

---
+++ src/main/java/com/hwq/fundamment/Thread/PCModel/JoinThread.java
@@ -0,0 +1,24 @@
+package com.hwq.fundamment.Thread.PCModel;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2020/1/22 0022 14:03
+ * @Description:
+ * 方法join()的作用是使所属的线程对象X正常执行run()方法中的任务
+ * 而使当前线程z进行无限期的阻塞，等待线程x销毁后再继续执行线程z后面的代码
+ * 方法join()具有使线程排队运行的作用，有些类似同步的运行效果；
+ * join与synchronized的区别是：
+ * join在内部使用wait()方法进行等待，而synchronized关键字使用的是"对象监视器"原理做为同步
+ */
+public class JoinThread extends Thread{
+    @Override
+    public void run() {
+        try {
+            int secondValue = (int)(Math.random() * 1000);
+            System.out.println(secondValue);
+            Thread.sleep(secondValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hwq/fundamment/Thread/PCModel/P.java
@@ -0,0 +1,33 @@
+package com.hwq.fundamment.Thread.PCModel;
+
+import com.hwq.fundamment.Thread.PCModel.Entity.ValueObject;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2020/1/22 0022 13:10
+ * @Description: 生产者
+ */
+public class P {
+     private String lock;
+     public P(String lock){
+         super();
+         this.lock = lock;
+     }
+
+     public void setValue() {
+         try {
+             synchronized (lock) {
+                 if (!ValueObject.value.equals("")) {
+                     System.out.println("生产者 "+Thread.currentThread().getName() + "WAITING了❤ ");
+                     lock.wait();
+                 }
+                 System.out.printf("生产者 " +Thread.currentThread().getName() + " RUNNABLE了 ");
+                 String value = System.currentTimeMillis() + "_" + System.nanoTime();
+                 ValueObject.value = value;
+                 lock.notifyAll();
+             }
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+     }
+}
