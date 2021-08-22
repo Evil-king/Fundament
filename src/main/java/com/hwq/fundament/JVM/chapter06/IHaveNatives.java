fab8bd0eab5b0da8e8c6671fa7c186f1bb52de80
“yhuwenqing “hwq8910@163,com <hwq8910@163.com>
2021-08-22 16:36:38 +0800

    update JVM

---
+++ src/main/java/com/hwq/fundament/JVM/chapter06/IHaveNatives.java
@@ -0,0 +1,16 @@
+package com.hwq.fundament.JVM.chapter06;
+
+/**
+ * @author shkstart
+ * @create 2020 下午 8:53
+ */
+public class IHaveNatives {
+    public native void Native1(int x);
+
+    public native static long Native2();
+
+    private native synchronized float Native3(Object o);
+
+    native void Native4(int[] ary) throws Exception;
+
+}
