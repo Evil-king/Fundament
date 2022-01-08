eb8d99db97a60ce2be030c1a55e917eb8c5dae98
timmy.hu <timmy.hu@lianwei.con.cn>
2022-01-08 20:28:17 +0800

    update

--- src/main/java/com/hwq/fundament/JVM/chapter11/MaxDirectMemorySizeTest.java
+++ src/main/java/com/hwq/fundament/JVM/chapter11/MaxDirectMemorySizeTest.java
@@ -1,7 +1,4 @@
 package com.hwq.fundament.JVM.chapter11;
-
-import sun.misc.Unsafe;
-
 import java.lang.reflect.Field;
 
 /**
@@ -9,16 +6,16 @@ import java.lang.reflect.Field;
  * @author shkstart  shkstart@126.com
  * @create 2020  0:36
  */
-public class MaxDirectMemorySizeTest {
-    private static final long _1MB = 1024 * 1024;
-
-    public static void main(String[] args) throws IllegalAccessException {
-        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
-        unsafeField.setAccessible(true);
-        Unsafe unsafe = (Unsafe)unsafeField.get(null);
-        while(true){
-            unsafe.allocateMemory(_1MB);
-        }
-
-    }
-}
+//public class MaxDirectMemorySizeTest {
+//    private static final long _1MB = 1024 * 1024;
+//
+//    public static void main(String[] args) throws IllegalAccessException {
+//        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+//        unsafeField.setAccessible(true);
+//        Unsafe unsafe = (Unsafe)unsafeField.get(null);
+//        while(true){
+//            unsafe.allocateMemory(_1MB);
+//        }
+//
+//    }
+//}
